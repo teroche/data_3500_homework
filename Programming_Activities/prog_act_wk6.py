13710b494560071bdd0512edea67c017a18ed72b
teroche <tarae.roche@usu.edu>
2026-02-13 22:01:01 +0000

    Programming Activities Complete

--- Programming_Activities/prog_act_wk6.py
+++ Programming_Activities/prog_act_wk6.py
@@ -1,1 +1,23 @@
+#Activity 1
+def welcome_fctn(name):
+    print("Welcome", name)
+welcome_fctn("Tara")
 
+#Activity 2
+def welcome_fctn(name):
+    welcome_message = "Welcome " + name
+    return welcome_message
+
+print(welcome_fctn("Tara"))
+
+#Activity 3
+def welcome_fctn(name, age, favorite_color):
+    welcome_message = (
+        "Welcome " + name +
+        ", you are " + str(age) +
+        " years old, and " + favorite_color +
+        " is your favorite color"
+    )
+    return welcome_message
+
+print(welcome_fctn("Tara", 47, "Purplegit"))
